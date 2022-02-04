4f03a1a911d7728ee6d66a59a61d0665e1ac4b14
jaroldhakins <stivenhakins@gmail.com>
2022-02-04 21:45:02 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,22 +6,23 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    req1 = requests.get(url + "users/" + argv[1])
-    employee = req1.json().get("name")
-    req2 = requests.get(url + 'todos?userId=' + argv[1]).json()
-    completed_tasks = 0
-    all_tasks = 0
-    title = []
-    if employee is not None:
-        for i in req2:
-            if i.get("completed") is True:
-                completed_tasks += 1
-                title.append(i.get("title"))
-            all_tasks += 1
-        print(
-            "Employee {} is done with tasks({}/{}):"
-            .format(employee, completed_tasks, all_tasks)
-            )
-        for j in title:
-            print("\t{}".format(j))
+    if len(argv) > 1:
+        url = 'https://jsonplaceholder.typicode.com/'
+        req1 = requests.get(url + "users/" + argv[1])
+        employee = req1.json().get("name")
+        req2 = requests.get(url + 'todos?userId=' + argv[1]).json()
+        completed_tasks = 0
+        all_tasks = 0
+        title = []
+        if employee is not None:
+            for i in req2:
+                if i.get("completed") is True:
+                    completed_tasks += 1
+                    title.append(i.get("title"))
+                all_tasks += 1
+            print(
+                "Employee {} is done with tasks({}/{}):"
+                .format(employee, completed_tasks, all_tasks)
+                )
+            for j in title:
+                print("\t{}".format(j))
