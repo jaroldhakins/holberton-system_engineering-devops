c083d552a136d77ca401452c0ada19b77c7c5632
jaroldhakins <stivenhakins@gmail.com>
2022-02-18 03:27:56 +0000

    0-strace_is_your_friend.pp

--- 0x18-webstack_monitoring/0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x18-webstack_monitoring/0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# strace - fixing
-exec { 'fixed-phpp':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => '/bin';
+# Fix the issue, (why is returning Internal server error 500) 
+exec { 'fix_settings_error':
+    command => 'sudo sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => ['/usr/bin', '/usr/sbin', '/usr/local/bin', '/usr/local/sbin'],
 }
